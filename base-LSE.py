ee543fe82f39034867a974ecc28634f25282900a
xhazen <gazambranoh@gmail.com>
2026-02-17 18:30:54 -0500

    Update base-LSE.py

--- base-LSE.py
+++ base-LSE.py
@@ -29,4 +29,26 @@ class ListaSE:
 			return
 		else:
 			nuevo_nodo.siguiente = self.cabeza
+
 			self.cabeza = nuevo_nodo
+
+	# Agregar al final
+	def agregarFinal(self, data):
+		nuevo_nodo = Nodo(data)
+		if self.cabeza is None:
+			self.cabeza = nuevo_nodo
+			return
+		
+        while actual.siguiente is not None:
+            actual = actual.siguiente
+		
+        actual.siguiente = nuevo_nodo
+	
+    # Contar elementos de la lista
+	def contar(self):
+		contador = 0
+		actual = self.cabeza
+		while actual is not None:
+			contador = contador+1
+			actual = actual.siguiente
+        return contador
